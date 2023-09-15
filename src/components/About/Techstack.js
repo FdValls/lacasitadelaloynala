05f427cc67a0cc386baab311c1d66180f5dd7126
Fdvalls <fernandodanielvalls@gmail.com>
2023-09-15 10:13:33 -0300

    final deploy

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -2,10 +2,15 @@ import { React, useState } from "react";
 import { Row } from "react-bootstrap";
 import atenas from "../../Assets/atenas.png";
 import negrito from "../../Assets/negrito.jpeg";
-import polonia from "../../Assets/polonia.jpeg";
+// import polonia from "../../Assets/polonia.jpeg";
+import polonia from "../../Assets/poloniaFinal.png";
 import lalo from "../../Assets/lalo.jpeg";
-import cali from "../../Assets/cali.jpeg";
+// import cali from "../../Assets/cali.jpeg";
+import cali from "../../Assets/Screenshot from 2023-09-15 10-07-06.png";
+import pipe from "../../Assets/Screenshot from 2023-09-15 10-11-59.png";
+import nalita from "../../Assets/nalitaFinal.png";
 import atenasUltima from "../../Assets/atenas-ultima.png";
+import lalitoFinal from "../../Assets/lalitoFinal.jpeg";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
@@ -26,113 +31,102 @@ function Techstack() {
   return (
     <body>
       <article className="contenedor">
-        <img  style={{ width: "120px", height: "140px" }} src={negrito}/>
-        <img  style={{ width: "120px", height: "140px" }} src={negrito}/>
-        <img  style={{ width: "120px", height: "140px" }} src={negrito}/>
-        <img  style={{ width: "120px", height: "140px" }} src={polonia}/>
-        <img  style={{ width: "120px", height: "140px" }} src={polonia}/>
+        {/* <img  style={{ width: "120px", height: "140px" }} src={negrito}/>
         <img  style={{ width: "120px", height: "140px" }} src={polonia}/>
         <img  style={{ width: "120px", height: "140px" }} src={polonia}/>
         <img  style={{ width: "120px", height: "140px" }} src={lalo}/>
-        <img  style={{ width: "120px", height: "140px" }} src={lalo}/>
-        <img  style={{ width: "120px", height: "140px" }} src={lalo}/>
         <img  style={{ width: "120px", height: "140px" }} src={cali}/>
         <img  style={{ width: "120px", height: "140px" }} src={cali}/>
         <img  style={{ width: "120px", height: "140px" }} src={cali}/>
         <img  style={{ width: "120px", height: "140px" }} src={cali}/>
         <img  style={{ width: "120px", height: "140px" }} src={cali}/>
-        <img  style={{ width: "120px", height: "140px" }} src={negrito}/>
-        {/* <div style={{border: "3px solid violet"}} class="planta">
-          <CardMedia 
+        <img  style={{ width: "120px", height: "140px" }} src={negrito}/> */}
+        <div
+          style={{ border: "3px solid violet", borderRadius: "15%" }}
+          class="violet-purple"
+        >
+          <CardMedia
             component="img"
-               style={{ width: "120px", height: "140px" }}            // height="250"
+            class="cardSize"
             // image={atenas}
             // image="https://raw.githubusercontent.com/InoveAlumnos/pokemon_assets_js/main/assets/001.png"
             image={atenasUltima}
             alt="green iguana"
           />
-          <h5 style={{fontSize: "13px"}}>Bulbasaur</h5>
+          <h5>Atenas</h5>
         </div>
-        <div style={{border: "3px solid violet"}} class="planta">
+        <div
+          style={{ border: "3px solid violet", borderRadius: "15%" }}
+          class="violet-purple"
+        >
           <CardMedia
             component="img"
-              style={{ width: "120px", height: "140px" }}           // height="250"
-            // image={https://raw.githubusercontent.com/InoveAlumnos/pokemon_assets_js/main/assets/002.png"}
-            image="https://raw.githubusercontent.com/InoveAlumnos/pokemon_assets_js/main/assets/001.png"
+            class="cardSize"
+            image={lalitoFinal}
             alt="green iguana"
           />
-          <h5 style={{fontSize: "13px"}}>Ivysaur</h5>
+          <h5>Puppy</h5>
         </div>
-        <div style={{border: "3px solid violet"}} class="planta">
+        <div
+          style={{ border: "3px solid violet", borderRadius: "15%" }}
+          class="violet-purple"
+        >
           <CardMedia
             component="img"
-              style={{ width: "120px", height: "140px" }}           // height="250"
-            // image={https://raw.githubusercontent.com/InoveAlumnos/pokemon_assets_js/main/assets/002.png"}
+            class="cardSize"
             image={negrito}
             alt="green iguana"
           />
-          <h5 style={{fontSize: "13px"}}>Venusaur</h5>
+          <h5>Negrito</h5>
         </div>
-        <div style={{border: "3px solid violet"}} class="fuego">
+        <div
+          style={{ border: "3px solid violet", borderRadius: "15%" }}
+          class="violet-purple"
+        >
           <CardMedia
             component="img"
-              style={{ width: "120px", height: "140px" }}           // height="250"
-            // image={https://raw.githubusercontent.com/InoveAlumnos/pokemon_assets_js/main/assets/002.png"}
-            image="https://raw.githubusercontent.com/InoveAlumnos/pokemon_assets_js/main/assets/004.png"
+            class="cardSize"
+            image={polonia}
             alt="green iguana"
           />
-          <h5 style={{fontSize: "13px"}}>Charmander</h5>
+          <h5>Polonia</h5>
         </div>
-        <div style={{border: "3px solid violet"}} class="fuego">
+        <div
+          style={{ border: "3px solid violet", borderRadius: "15%" }}
+          class="violet-purple"
+        >
           <CardMedia
             component="img"
-              style={{ width: "120px", height: "140px" }}           // height="250"
-            // image={https://raw.githubusercontent.com/InoveAlumnos/pokemon_assets_js/main/assets/002.png"}
-            image="https://raw.githubusercontent.com/InoveAlumnos/pokemon_assets_js/main/assets/005.png"
+            class="cardSize"
+            image={cali}
             alt="green iguana"
           />
-          <h5 style={{fontSize: "13px"}}>Charmeleon</h5>
+          <h5>Cali</h5>
         </div>
-        <div style={{border: "3px solid violet"}} class="fuego">
+        <div
+          style={{ border: "3px solid violet", borderRadius: "15%" }}
+          class="violet-purple"
+        >
           <CardMedia
             component="img"
-              style={{ width: "120px", height: "140px" }}           // height="250"
-            // image={https://raw.githubusercontent.com/InoveAlumnos/pokemon_assets_js/main/assets/002.png"}
-            image="https://raw.githubusercontent.com/InoveAlumnos/pokemon_assets_js/main/assets/006.png"
+            class="cardSize"
+            image={nalita}
             alt="green iguana"
           />
-          <h5 style={{fontSize: "13px"}}>Charizard</h5>
+          <h5>Nalita</h5>
         </div>
-        <div style={{border: "3px solid violet"}} class="agua">
+        <div
+          style={{ border: "3px solid violet", borderRadius: "15%" }}
+          class="violet-purple"
+        >
           <CardMedia
             component="img"
-              style={{ width: "120px", height: "140px" }}           // height="250"
-            // image={https://raw.githubusercontent.com/InoveAlumnos/pokemon_assets_js/main/assets/002.png"}
-            image="https://raw.githubusercontent.com/InoveAlumnos/pokemon_assets_js/main/assets/007.png"
+            class="cardSize"
+            image={pipe}
             alt="green iguana"
           />
-          <h5 style={{fontSize: "13px"}}>Squirtle</h5>
+          <h5>Nalita</h5>
         </div>
-        <div style={{border: "3px solid violet"}} class="agua">
-          <CardMedia
-            component="img"
-              style={{ width: "120px", height: "140px" }}           // height="250"
-            // image={https://raw.githubusercontent.com/InoveAlumnos/pokemon_assets_js/main/assets/002.png"}
-            image="https://raw.githubusercontent.com/InoveAlumnos/pokemon_assets_js/main/assets/008.png"
-            alt="green iguana"
-          />
-          <h5 style={{fontSize: "13px"}}>Wartortle</h5>
-        </div>
-        <div style={{border: "3px solid violet"}} class="agua">
-          <CardMedia
-            component="img"
-              style={{ width: "120px", height: "140px" }}           // height="250"
-            // image={https://raw.githubusercontent.com/InoveAlumnos/pokemon_assets_js/main/assets/002.png"}
-            image="https://raw.githubusercontent.com/InoveAlumnos/pokemon_assets_js/main/assets/009.png"
-            alt="green iguana"
-          />
-          <h5 style={{fontSize: "13px"}}>Blastoise</h5>
-        </div> */}
       </article>
     </body>
   );
